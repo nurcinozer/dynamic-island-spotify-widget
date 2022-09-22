8a5dd09763c7425bed8d4ac26caa70d4930364fa
Nurçin <nrcinozer@gmail.com>
2022-09-23 01:18:29 +0300

    updated import and export

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { SpotifyWidget } from "./lib/modules";
+import { SpotifyWidget } from "./lib";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
---
+++ src/lib/index.ts
@@ -0,0 +1,3 @@
+import { SpotifyWidget } from "./modules/SpotifyWidget";
+
+export { SpotifyWidget };
